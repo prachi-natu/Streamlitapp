71c997afa46176c153f6eddc5d6a1dd5b634ec06
prachi-natu <prachinatuproject@gmail.com>
2021-03-21 12:18:32 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh and && streamlit run streamlit_app01.py
---
+++ streamlit_app01.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Welcome to this page!")
+st.subheader("This is my first Streamlit App")
+st.text("This is the text")
