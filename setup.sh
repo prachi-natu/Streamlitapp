4b753dc4470d5089d47d649beae8cda5e9953d4c
prachi-natu <prachinatuproject@gmail.com>
2021-03-22 10:34:57 +0530

    setup commit

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
-mkdir -p ~/ .streamlit/
+mkdir -p ~/ .streamlit
 
-echo "\
-[general]\n
-email = \prachimldl@gmail.com\"\n\
-" > ~/ .streamlit/config.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS = false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/ .streamlit/config.toml
